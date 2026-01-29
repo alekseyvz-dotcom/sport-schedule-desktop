ee23d903194aec80fd785c64b6f4322e7a0d9066
alekseyvz-dotcom <aleksey_vz@mail.ru>
2026-01-29 10:44:23 +0300

    Update main_window.py

--- app/ui/main_window.py
+++ app/ui/main_window.py
@@ -3,6 +3,7 @@ from app.services.users_service import AuthUser
 from app.ui.tenants_page import TenantsPage
 from app.ui.orgs_venues_page import OrgsVenuesPage
 from app.ui.schedule_page import SchedulePage
+from app.ui.org_usage_page import OrgUsagePage
 
 class MainWindow(QMainWindow):
     def __init__(self, user: AuthUser):
@@ -13,5 +14,6 @@ class MainWindow(QMainWindow):
         tabs.addTab(TenantsPage(), "Контрагенты")
         tabs.addTab(OrgsVenuesPage(), "Учреждения и площадки")
         tabs.addTab(SchedulePage(), "Расписание")
+        tabs.addTab(OrgUsagePage(self), "Загрузка учреждений")
 
         self.setCentralWidget(tabs)
