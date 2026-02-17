7b3187ab09da918cf41954cfc1272ea2357691ee
alekseyvz-dotcom <aleksey_vz@mail.ru>
2026-02-17 11:41:56 +0300

    Update main.py

--- web/main.py
+++ web/main.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
 from web.routers import auth, schedule, api
+from app.db import init_pool
 
 import os
 
@@ -19,6 +20,11 @@ app.mount("/static", StaticFiles(directory="web/static"), name="static")
 app.include_router(auth.router)
 app.include_router(schedule.router)
 app.include_router(api.router, prefix="/api/v1")
+
+
+@app.on_event("startup")
+def startup():
+    init_pool()
 
 
 @app.get("/health")
